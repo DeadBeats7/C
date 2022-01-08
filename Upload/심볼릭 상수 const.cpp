752ebc7043bac7c76be27c023280a0674b57dcfe
haekeun park <hanny137@naver.com>
2022-01-08 17:35:08 +0900

    .

---
+++ Upload/심볼릭 상수 const.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	const int NUM = 100;
+	const double PT = 3.14; //심볼릭 상수의 선언과 동시에 초기화
+
+	// NUM = 200;
+	// PT = 4.14;
+
+	return 0;
+}
---
+++ Upload/심볼릭 상수 define.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define PT 3.14
+#define NUM 100
+#define BUFFER_SIZE 200
+
+int main()
+{
+	printf("%lf \n", PT);
+	printf("%d \n", NUM);
+	printf("%d \n", BUFFER_SIZE);
+
+	return 0;
+}
