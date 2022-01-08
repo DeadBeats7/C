5d1e7be5e038a31192c20aaeca7a44a1bd2f175e
haekeun park <hanny137@naver.com>
2022-01-08 16:12:38 +0900

    .

---
+++ Upload/실수형 상수.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("실수형 상수 %lf + %lf = %lf입니다. \n", 4.2, 5.5, 4.2 + 5.5);
+
+	return 0;
+}
---
+++ Upload/정수형 상수.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("10진수 정수형 상수 %d + %d = %d입니다. \n", 20, 30, 20 + 30);
+	printf("16진수 정수형 상수 %x + %x = %x입니다. \n", 0x20, 0x30, 0x20 + 0x30);
+	printf("8진수 정수형 상수 %o + %o = %o입니다. \n", 020, 030, 020 + 030);
+
+	return 0;
+}
